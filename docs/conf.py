92253ece4eb0b2a49df970a5fc494d77deb7f284
Massimo Rossello <ajbum6g7q@mozmail.com>
2026-02-01 16:01:07 +0100

    Revert python versioning. Automated sha

--- docs/conf.py
+++ docs/conf.py
@@ -22,9 +22,9 @@ project = 'robotframework-clang'
 copyright = '2025- Massimo Rossello'
 author = 'Massimo Rossello'
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.4'
 
-version = '0.2.3'
+version = '0.2.4'
 root_doc = 'index'
 
 templates_path = ['_templates']
